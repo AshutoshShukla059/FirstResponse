5b74da9bfdfc011c1cd00d776ef36aaa361da939
AshutoshShukla059 <a.shuklaofficials01@gmail.com>
2022-05-02 10:00:42 +0530

    third text

--- Demo1MavenEg/src/main/java/com/lti/demos/MvenEx.java
+++ Demo1MavenEg/src/main/java/com/lti/demos/MvenEx.java
@@ -4,6 +4,7 @@ public class MvenEx {
 	public void main (String args[]) {
 		System.out.println("This is for Jenkin");
 		System.out.println("Jenkin's task is done");
+		System.out.println("Jenkin is done");
 		
 	}
 }
