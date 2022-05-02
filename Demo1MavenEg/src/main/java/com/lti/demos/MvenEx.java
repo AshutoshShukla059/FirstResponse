c3b70d473b15a402fc89453804a785f5946bfe62
AshutoshShukla059 <a.shuklaofficials01@gmail.com>
2022-05-02 09:58:55 +0530

    Second commit

--- Demo1MavenEg/src/main/java/com/lti/demos/MvenEx.java
+++ Demo1MavenEg/src/main/java/com/lti/demos/MvenEx.java
@@ -1,5 +1,9 @@
 package com.lti.demos;
 
 public class MvenEx {
-
+	public void main (String args[]) {
+		System.out.println("This is for Jenkin");
+		System.out.println("Jenkin's task is done");
+		
+	}
 }
